da1cecba9b3190d1eebcc3f56626c2dc255b11df
Max Irwin <max_irwin@yahoo.com>
2013-08-26 12:45:44 +0100

    Update polar140.js
    golfed some bytes for polar distance

--- public/javascripts/polar140.js
+++ public/javascripts/polar140.js
@@ -4,4 +4,4 @@ function(d,l,M){M=Math;return[M.floor(l*M.cos(d*2*M.PI/360))*d>90||d<270?1:-1,M.
 
 //Converts cartesian coordinates to polar 
 var polar = 
-function(x,y,M){M=Math;return[M.atan(y/x)*360/2/M.PI+(x<0&&y>=0)||(x<0&&y<0)?180:x>0&&y<0?360:0,M.pow((M.pow(x,2)+M.pow(y,2)),0.5)]}
+function(x,y,M){M=Math;return[M.atan(y/x)*360/2/M.PI+(x<0&&y>=0)||(x<0&&y<0)?180:x>0&&y<0?360:0,M.pow(x*x+y*y,0.5)]}
